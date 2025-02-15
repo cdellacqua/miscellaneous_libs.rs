8769cc7e31d95654df010336b56a44758ef305dc
cdellacqua <carlo.dellacqua97@gmail.com>
2025-02-15 22:35:53 +0100

    feat: add Goertzel analyzer

---
+++ audio.rs/src/analysis/fft/goertzel_analyzer.rs
@@ -0,0 +1,118 @@
+use std::{f32::consts::TAU, sync::Arc};
+
+use rustfft::num_complex::Complex32;
+
+use crate::analysis::WindowingFn;
+
+use super::FftBinPoint;
+
+#[derive(Clone)]
+pub struct GoertzelAnalyzer<const SAMPLE_RATE: usize, const SAMPLES_PER_WINDOW: usize> {
+	windowing_fn: Arc<dyn WindowingFn + Sync + Send + 'static>,
+	cur_transform_bins: Vec<FftBinPoint<SAMPLE_RATE, SAMPLES_PER_WINDOW>>,
+	frequency_bins: Vec<usize>,
+	coefficients: Vec<(f32, Complex32)>,
+}
+
+impl<const SAMPLE_RATE: usize, const SAMPLES_PER_WINDOW: usize> std::fmt::Debug
+	for GoertzelAnalyzer<SAMPLE_RATE, SAMPLES_PER_WINDOW>
+{
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.debug_struct(&format!(
+			"GoertzelAnalyzer<{SAMPLE_RATE}, {SAMPLES_PER_WINDOW}>"
+		))
+		.field("windowing_fn", &"omitted")
+		.field("cur_transform_bins", &self.cur_transform_bins)
+		.field("frequency_bins", &self.frequency_bins)
+		.field("coefficients", &self.coefficients)
+		.finish()
+	}
+}
+
+impl<const SAMPLE_RATE: usize, const SAMPLES_PER_WINDOW: usize>
+	GoertzelAnalyzer<SAMPLE_RATE, SAMPLES_PER_WINDOW>
+{
+	#[allow(clippy::cast_precision_loss)]
+	pub fn new(
+		mut frequency_bins: Vec<usize>,
+		windowing_fn: impl WindowingFn + Send + Sync + 'static,
+	) -> Self {
+		frequency_bins.sort_unstable();
+		Self {
+			// Pre-computing coefficients
+			coefficients: frequency_bins
+				.iter()
+				.map(|&bin| {
+					let ω = TAU * bin as f32 / SAMPLES_PER_WINDOW as f32;
+					(2.0 * ω.cos(), Complex32::new(ω.cos(), ω.sin()))
+				})
+				.collect(),
+			cur_transform_bins: vec![FftBinPoint::default(); frequency_bins.len()],
+			frequency_bins,
+			windowing_fn: Arc::new(windowing_fn),
+		}
+	}
+
+	/// Analyze a signal in the domain of time, sampled at the configured sample rate.
+	///
+	/// The returned `Vec` is sorted by frequency bin.
+	///
+	/// # Panics
+	/// - if the passed `signal` is not compatible with the configured `samples_per_window`.
+	#[must_use]
+	pub fn analyze_bins(
+		&mut self,
+		signal: &[f32],
+	) -> &Vec<FftBinPoint<SAMPLE_RATE, SAMPLES_PER_WINDOW>> {
+		let samples = signal.len();
+
+		assert_eq!(
+			samples, SAMPLES_PER_WINDOW,
+			"signal with incompatible length received"
+		);
+
+		// Normalization also applies here.
+		// https://docs.rs/rustfft/6.2.0/rustfft/index.html#normalization
+		#[allow(clippy::cast_precision_loss)]
+		let normalization_factor = 1.0 / (samples as f32).sqrt();
+
+		let windowed_signal: Vec<f32> = signal
+			.iter()
+			.enumerate()
+			.map(|(i, &sample)| sample * (self.windowing_fn).ratio_at(i, SAMPLES_PER_WINDOW))
+			.collect();
+
+		for ((&bin, coeff), bin_point) in self
+			.frequency_bins
+			.iter()
+			.zip(self.coefficients.iter())
+			.zip(self.cur_transform_bins.iter_mut())
+		{
+			let mut z1 = 0.0;
+			let mut z2 = 0.0;
+
+			for sample in &windowed_signal {
+				let z0 = sample + coeff.0 * z1 - z2;
+				z2 = z1;
+				z1 = z0;
+			}
+
+			*bin_point = FftBinPoint {
+				c: Complex32::new(z1 * coeff.1.re - z2, z1 * coeff.1.im) * normalization_factor,
+				frequency_idx: bin,
+			};
+		}
+
+		&self.cur_transform_bins
+	}
+
+	#[must_use]
+	pub fn sample_rate(&self) -> usize {
+		SAMPLE_RATE
+	}
+
+	#[must_use]
+	pub fn samples_per_window(&self) -> usize {
+		SAMPLES_PER_WINDOW
+	}
+}
--- audio.rs/src/analysis/fft/mod.rs
+++ audio.rs/src/analysis/fft/mod.rs
@@ -6,3 +6,72 @@ pub use fft_point::*;
 
 mod stft_analyzer;
 pub use stft_analyzer::*;
+
+mod goertzel_analyzer;
+pub use goertzel_analyzer::*;
+
+#[cfg(test)]
+mod tests {
+	use std::f32::consts::TAU;
+
+	use crate::{
+		analysis::{
+			fft::{frequency_to_index, GoertzelAnalyzer, StftAnalyzer},
+			windowing_fns::HannWindow,
+		},
+		output::frequencies_to_samples,
+		NOfSamples,
+	};
+
+	#[test]
+	fn cross_check_goertzel_and_stft() {
+		const SAMPLE_RATE: usize = 44100;
+		const SAMPLES_RAW: usize = 44100;
+		const SAMPLES: NOfSamples<SAMPLE_RATE> = NOfSamples::new(SAMPLES_RAW);
+
+		let frequency = 440.;
+		let frequency_bin = frequency_to_index(frequency, SAMPLES);
+
+		let signal = frequencies_to_samples(SAMPLES, &[frequency]);
+		let signal = signal.as_mono();
+		let mut goertzel: GoertzelAnalyzer<SAMPLE_RATE, SAMPLES_RAW> = GoertzelAnalyzer::new(
+			vec![
+				frequency_bin - 20,
+				frequency_bin - 15,
+				frequency_bin - 10,
+				frequency_bin - 5,
+				frequency_bin,
+				frequency_bin + 5,
+				frequency_bin + 10,
+				frequency_bin + 15,
+				frequency_bin + 20,
+			],
+			HannWindow::new(),
+		);
+		let mut stft: StftAnalyzer<SAMPLE_RATE, SAMPLES_RAW> = StftAnalyzer::new(HannWindow::new());
+
+		let stft_result = stft
+			.analyze_bins(signal)
+			.iter()
+			.max_by(|a, b| a.norm_sqr().total_cmp(&b.norm_sqr()))
+			.unwrap();
+		let goertzel_result = goertzel
+			.analyze_bins(signal)
+			.iter()
+			.max_by(|a, b| a.norm_sqr().total_cmp(&b.norm_sqr()))
+			.unwrap();
+
+		assert_eq!(
+			stft_result.frequency_idx, goertzel_result.frequency_idx,
+			"goertzel and stft should yield the same frequency result"
+		);
+		assert!(
+			(stft_result.c.norm() - goertzel_result.c.norm()).abs() < 0.01,
+			"goertzel and stft should yield a similar magnitude result"
+		);
+		assert!(
+			(stft_result.c.arg() - goertzel_result.c.arg()).abs() < TAU / 100.,
+			"goertzel and stft should yield a similar phase result"
+		);
+	}
+}
