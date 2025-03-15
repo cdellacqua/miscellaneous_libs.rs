cd2b05a351278364fc30ae1108946e195ecee260
cdellacqua <carlo.dellacqua97@gmail.com>
2025-03-15 17:04:34 +0100

    feat: add From impl for Harmonic/DiscreteHarmonic

--- audio.rs/src/analysis/mod.rs
+++ audio.rs/src/analysis/mod.rs
@@ -13,3 +13,19 @@ pub use harmonic::*;
 
 mod frequency_bin;
 pub use frequency_bin::*;
+
+impl<const SAMPLE_RATE: usize, const SAMPLES_PER_WINDOW: usize> From<Harmonic>
+	for DiscreteHarmonic<SAMPLE_RATE, SAMPLES_PER_WINDOW>
+{
+	fn from(value: Harmonic) -> Self {
+		Self::from_frequency(value.phasor(), value.frequency())
+	}
+}
+
+impl<const SAMPLE_RATE: usize, const SAMPLES_PER_WINDOW: usize>
+	From<DiscreteHarmonic<SAMPLE_RATE, SAMPLES_PER_WINDOW>> for Harmonic
+{
+	fn from(value: DiscreteHarmonic<SAMPLE_RATE, SAMPLES_PER_WINDOW>) -> Self {
+		Self::new(value.phasor(), value.frequency())
+	}
+}
