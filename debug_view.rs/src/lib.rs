36ee45ce8764d33222a38fdd2cb8e6225c0a2d55
cdellacqua <carlo.dellacqua97@gmail.com>
2025-05-19 21:36:46 +0200

    feat: provide macroquad prelude in macro block

--- debug_view.rs/src/lib.rs
+++ debug_view.rs/src/lib.rs
@@ -7,6 +7,8 @@ pub use macroquad;
 macro_rules! if_debug_view {
 	($code:block) => {
 		#[cfg(debug_assertions)]
+		use debug_view::macroquad::prelude::*;
+		#[cfg(debug_assertions)]
 		{
 			$code
 		}
@@ -19,6 +21,7 @@ macro_rules! draw_debug_view_frame {
 		debug_view::if_debug_view!({
 			use mutex_ext::LockExt;
 			debug_view::DEBUG_VIEW.with_lock_mut(move |debug_view| {
+				use debug_view::macroquad::prelude::*;
 				debug_view.run(Box::new(move || $code));
 			});
 		})
